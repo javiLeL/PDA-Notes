9c01c41a3ee8aeb4c7899561c8ce243a0cf139ea
Javi.LeL <javier212002@gmail.com>
2024-01-25 16:58:55 +0100

    añadiendo una spash screen

---
+++ app/src/main/java/com/example/pdanotes/SpashActivity.java
@@ -0,0 +1,41 @@
+package com.example.pdanotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+public class SpashActivity extends AppCompatActivity {
+    final int tiempo = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ProgressBar progressBar = findViewById(R.id.progressBar);
+                for (int i=0;i<=100;i++){
+                    try {
+                        Thread.currentThread().sleep((tiempo-100)/100);
+                    }catch (Exception e){
+
+                    }
+                    progressBar.setProgress(i, true);
+                }
+            }
+        }).start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, tiempo);
+
+    }
+}
